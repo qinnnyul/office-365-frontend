f0348a667aea0b4300add8092f1997f9706a593d
polang <grzx2210@163.com>
2018-04-30 21:06:35 +0800

    [greengerong] office online host

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Office from './Office';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">审计师桌面系统</h1>
+                </header>
+                <div className="App-intro">
+                    <Office></Office>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/Office.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+const VIEW_ACTION = 'https://word-view.officeapps-df.live.com/wv/wordviewerframe.aspx?<ui=UI_LLCC&><rs=DC_LLCC&><dchat=DISABLE_CHAT&><hid=HOST_SESSION_ID&><showpagestats=PERFSTATS&><IsLicensedUser=BUSINESS_USER&><actnavid=ACTIVITY_NAVIGATION_ID&>';
+const OFFICE_ONLINE_ACTION_URL = "wait";
+const ACCESS_TOKEN_VALUE = "token";
+const ACCESS_TOKEN_TTL_VALUE = "ttl";
+
+class Office extends Component {
+    render() {
+        return (
+            <div className="office">
+                <form id="office_form" ref={el => this.office_form = el} name="office_form" target="office_frame"
+                      action={OFFICE_ONLINE_ACTION_URL} method="post">
+                    <input name="access_token" value={ACCESS_TOKEN_VALUE} type="hidden"/>
+                    <input name="access_token_ttl" value={ACCESS_TOKEN_TTL_VALUE} type="hidden"/>
+                </form>
+                <span id="frameholder" ref={el => this.frameholder = el}></span>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        const office_frame = document.createElement('iframe');
+        office_frame.name = 'office_frame';
+        office_frame.id = 'office_frame';
+        office_frame.title = 'Office Online';
+        office_frame.setAttribute('allowfullscreen', 'true');
+        this.frameholder.appendChild(office_frame);
+        this.office_form.submit();
+    }
+
+}
+
+export default Office;
